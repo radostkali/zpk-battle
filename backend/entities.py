b5dda8c37c7b98e4d649c98270c76e1f0fdf8312
Taras Rodionov <t.rodionov@admitad.com>
2021-02-02 23:31:19 +0300

    Create entities

--- backend/entities.py
+++ backend/entities.py
@@ -1,8 +1,45 @@
-import dataclasses
+from datetime import date
+from dataclasses import dataclass
+from typing import Optional
 
 
-@dataclasses.dataclass
+@dataclass
 class UserEntity:
     pk: int
     username: str
     password: str
+
+
+@dataclass
+class RateCategoryEntity:
+    id: int
+    name: str
+
+
+@dataclass
+class RoundEntity:
+    id: int
+    number: int
+    theme: str
+    type: str
+    last_day: date
+    style: Optional[str] = None
+
+    def __hash__(self):
+        return hash(self.id)
+
+
+@dataclass
+class TrackEntity:
+    id: int
+    name: str
+    user_id: int
+    user_username: str
+
+
+@dataclass
+class RateEntity:
+    category_id: int
+    user_id: int
+    track_id: int
+    user_username: str
