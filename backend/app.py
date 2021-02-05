14e9a0f98a5016714e82ec217f90b50c49a7b9fb
Taras Rodionov <t.rodionov@admitad.com>
2021-02-05 19:45:25 +0300

    Fix config

--- backend/app.py
+++ backend/app.py
@@ -13,9 +13,10 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
+    flask_env = os.environ.get('FLASK_ENV')
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_ECHO'] = True if flask_env == 'development' else False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
